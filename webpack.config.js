a7542099976e8b3d957d659357aa6a123fce34b3
Roberto Bravo <robertbass@hotmail.com>
2023-05-22 10:22:37 -0500

    Fix typo

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const copywebpackplugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -31,7 +31,7 @@ module.exports = {
             }
         ),
 
-        new copywebpackplugin([{
+        new CopyWebpackPlugin([{
             from: './src/styles/styles.css',
             to: ''
         }])
